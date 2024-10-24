bc475d7e508b2870d5dbfe1923864d67c80f5cfc
Ngon <vucongngon219@gmail.com>
2024-10-24 10:42:45 +0700

    add: Banner home page

--- Do_an_ticket_box/obj/Debug/net6.0/Do_an_ticket_box.AssemblyInfo.cs
+++ Do_an_ticket_box/obj/Debug/net6.0/Do_an_ticket_box.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do_an_ticket_box")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58100c8adc26f8924a718fdc9987158acbf401dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b304189fc851da04353aecdff04eff32269de8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do_an_ticket_box")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do_an_ticket_box")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
