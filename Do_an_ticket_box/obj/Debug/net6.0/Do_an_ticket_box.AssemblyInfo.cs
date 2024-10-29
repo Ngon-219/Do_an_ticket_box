f32a2bfda9efbc53f9153153fea8772e5cbed26d
nguyendungnd204 <125289761+nguyendungnd204@users.noreply.github.com>
2024-10-29 15:53:07 +0700

    Acconut-view-controller

--- Do_an_ticket_box/obj/Debug/net6.0/Do_an_ticket_box.AssemblyInfo.cs
+++ Do_an_ticket_box/obj/Debug/net6.0/Do_an_ticket_box.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do_an_ticket_box")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250ad223f0cbecc5d161c319e0dede5cc734110c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e1782c556ddd20ab06c6448b22a19eddffa181")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do_an_ticket_box")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do_an_ticket_box")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
