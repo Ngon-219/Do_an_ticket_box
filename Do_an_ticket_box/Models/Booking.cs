4e30e89c139461294543c83173a28c0d6156e31b
Ngon <vucongngon219@gmail.com>
2024-11-04 23:25:46 +0700

    add: gender to database User

--- Do_an_ticket_box/Models/Booking.cs
+++ Do_an_ticket_box/Models/Booking.cs
@@ -9,18 +9,18 @@ namespace Do_an_ticket_box.Models
         [Key]
         public int Booking_ID { get; set; }
         [ForeignKey("User")]
-        public int User_ID { get; set; }
+        public int? User_ID { get; set; }
         [ForeignKey("Event")]
-        public int Event_ID { get; set; }
+        public int? Event_ID { get; set; }
         [ForeignKey("Ticket")]
-        public int Ticket_ID { get; set; }
+        public int? Ticket_ID { get; set; }
         [Column("Booking_time", TypeName =  "timestamp")]
         public DateTime booking_time { get; set; }
         [Column("Total_amout", TypeName = "decimal")]
-        public decimal total_amout { get; set; }
+        public decimal? total_amout { get; set; }
         [Column("Status", TypeName = "nvarchar(20)")]
-        public string status { get; set; }
-        public Payment Payment { get; set; }
-        public User User { get; set; }
+        public string? status { get; set; }
+        public Payment? Payment { get; set; }
+        public User? User { get; set; }
     }
 }
