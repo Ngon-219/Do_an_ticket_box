ea0a50bba7c1d78550e48a4df7db3877c64bc24c
HoangDuongHuy <duonghuyhoanghuce@gmail.com>
2024-11-10 17:14:46 +0700

    DangKyDangNhapva1itformEvent

---
+++ Do_an_ticket_box/Controllers/CreateEventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Do_an_ticket_box.Controllers
+{
+    public class CreateEventController : Controller
+    {
+        public IActionResult EventCreated()
+        {
+            return View();
+        }
+    }
+}
---
+++ Do_an_ticket_box/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Do_an_ticket_box.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email là bắt buộc.")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu là bắt buộc.")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Xác nhận mật khẩu là bắt buộc.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Mật khẩu xác nhận không khớp.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
