61bdd3cef83b6502e310ed486c4fdd948548305f
nguyendungnd204 <125289761+nguyendungnd204@users.noreply.github.com>
2024-11-11 11:16:32 +0700

    Remove cached files based on .gitignore

--- Do_an_ticket_box/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Do_an_ticket_box.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
