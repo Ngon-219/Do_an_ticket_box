adb3ceb76fd89fe2c5122ab1e9fc3802953618d6
Ngon <vucongngon219@gmail.com>
2024-11-17 23:40:22 +0700

    fix: useremail

--- Do_an_ticket_box/Controllers/SearchController.cs
+++ Do_an_ticket_box/Controllers/SearchController.cs
@@ -20,38 +20,10 @@ namespace Do_an_ticket_box.Controllers
         // GET: Search
         public IActionResult Index(string query)
         {
-            ViewBag.Query = query;
-
-            // Lấy danh sách sự kiện và vé từ cơ sở dữ liệu
-            var events = _context.Events.ToList();
-            var tickets = _context.Ticket.ToList();
-
-            // Thực hiện tìm kiếm
-            var model = (from e in events
-                         join t in tickets on e.Event_ID equals t.Event_ID
-                         where string.IsNullOrEmpty(query) || e.Event_Name.ToLower().Contains(query.ToLower())
-                         select new Do_an_ticket_box.Models.EventTicketViewModel
-                         {
-                             Event_ID = e.Event_ID,
-                             Event_image = e.event_image,
-                             Event_Name = e.Event_Name,
-                             Price = t.price,
-                             Event_Date = e.Event_date
-                         }).ToList();
-
-            var allTickets = (from t in tickets
-                              join e in events on t.Event_ID equals e.Event_ID
-                              select new Do_an_ticket_box.Models.EventTicketViewModel
-                              {
-                                  Event_ID = e.Event_ID,
-                                  Event_image = e.event_image,
-                                  Event_Name = e.Event_Name,
-                                  Price = t.price,
-                                  Event_Date = e.Event_date
-                              }).ToList();
-
-            ViewBag.AllTickets = allTickets;
-            return View(model);
+            var result = this._context.Events
+                        .Where(r => r.Event_Name.ToLower().Contains(query) || r.location.ToLower().Contains(query) || r.description.ToLower().Contains(query))
+                        .ToList();
+            return View(result);
 
         }
 
